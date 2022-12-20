9697fee1bb1e0c299eec6ca1913266e2534d3f5d
Okan Ecevit <okan@ecevit.at>
2022-12-20 21:27:47 +0100

    day04p02

---
+++ 2022/day04/p02.py
@@ -0,0 +1,14 @@
+
+
+c = 0
+for pairs in open("input.txt", "r"):
+    one, two, three, four = map(int, pairs.replace("-", ",").split(","))
+    
+    if one <= three and two >= four or three <= one and four >= two:
+        c +=1
+    elif one == four or two == three:
+        c += 1
+    
+
+print(c)
+
